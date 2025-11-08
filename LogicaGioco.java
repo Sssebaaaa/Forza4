aaf97cef4b1d4987bc1c5701e5efbf405f4d0824
dxvideee <davide.mallardo08@icloud.com>
2025-11-08 22:49:01 +0100

    Update LogicaGioco.java

---
+++ LogicaGioco.java
@@ -0,0 +1,17 @@
+public class LogicaGioco {
+
+    private char[][] tabellone = new char[6][7];
+
+    public void inizializzaTabellone(){
+        for(int i=0; i<tabellone.length; i++){
+            for(int j=0; j<tabellone[i].length; j++){
+                tabellone[i][j] = ' ';
+            }
+        }
+    }
+    
+
+     
+
+
+}
