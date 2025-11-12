9493d47d242ed415396ceb4560ef501b012b41ef
Ssebaaa <nicholassebastiani70@gmail.com>
2025-11-12 18:30:50 +0100

    Update Bot.java
    difficolta media, semplici cicli che scorrono il tabellone e vedono cosa fare

--- Bot.java
+++ Bot.java
@@ -20,3 +20,21 @@ public class Bot {
         while (tab[0][col] != ' ');
         return col;
     }
+
+    private int mossaMedia(char[][] tab, char bot, char avv) {
+        for (int c = 0; c < 7; c++) {
+            if (tab[0][c] == ' ') {
+                char[][] sim = simula(tab, c, bot);
+                if (controllo.controllaVittoria(sim, bot)) return c;
+            }
+        }
+        for (int c = 0; c < 7; c++) {
+            if (tab[0][c] == ' ') {
+                char[][] sim = simula(tab, c, avv);
+                if (controllo.controllaVittoria(sim, avv)) return c;
+            }
+        }
+        int[] priorita = {3, 2, 4, 1, 5, 0, 6};
+        for (int c : priorita) if (tab[0][c] == ' ') return c;
+        return mossaRandom(tab);
+    }
