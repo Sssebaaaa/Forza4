bb682527ed471d88fd6ccb01113d6db706725309
Ssebaaa <nicholassebastiani70@gmail.com>
2025-11-12 19:38:16 +0100

    aggiornamento bot.java
    una volta cambiato il controllovittoria aggiorniamo bot.java in modo che non dipenda più da quel file ma da quello nuovo appena committato
    
    Co-Authored-By: dxvideee <194516644+dxvideee@users.noreply.github.com>

--- Bot.java
+++ Bot.java
@@ -1,41 +1,51 @@
 public class Bot {
 
-    private ControlloVittoria controllo = new ControlloVittoria();
-
     public int giocaTurnoBot(LogicaGioco logica, char bot, char avversario, String difficolta) {
         char[][] tab = copiaTabella(logica.getTabellone());
 
-        if (difficolta.equalsIgnoreCase("facile"))
-            return mossaRandom(tab);
-
-        if (difficolta.equalsIgnoreCase("media"))
-            return mossaMedia(tab, bot, avversario);
-
-        return mossaDifficile(tab, bot, avversario);
+        switch (difficolta.toLowerCase()) {
+            case "facile":
+                return mossaRandom(tab);
+            case "media":
+                return mossaMedia(tab, bot, avversario);
+            case "difficile":
+                return mossaDifficile(tab, bot, avversario);
+            default:
+                return mossaRandom(tab);
+        }
     }
 
     private int mossaRandom(char[][] tab) {
         int col;
-        do col = (int) (Math.random() * 7);
-        while (tab[0][col] != ' ');
+        do {
+            col = (int) (Math.random() * 7);
+        } while (tab[0][col] != ' ');
         return col;
     }
 
     private int mossaMedia(char[][] tab, char bot, char avv) {
+        // Controlla se può vincere subito
         for (int c = 0; c < 7; c++) {
             if (tab[0][c] == ' ') {
                 char[][] sim = simula(tab, c, bot);
-                if (controllo.controllaVittoria(sim, bot)) return c;
+                if (controlloVittoria(sim, bot)) return c;
             }
         }
+
+        // Blocca l'avversario se può vincere
         for (int c = 0; c < 7; c++) {
             if (tab[0][c] == ' ') {
                 char[][] sim = simula(tab, c, avv);
-                if (controllo.controllaVittoria(sim, avv)) return c;
+                if (controlloVittoria(sim, avv)) return c;
             }
         }
+
+        // Priorità alle colonne centrali
         int[] priorita = {3, 2, 4, 1, 5, 0, 6};
-        for (int c : priorita) if (tab[0][c] == ' ') return c;
+        for (int c : priorita) {
+            if (tab[0][c] == ' ') return c;
+        }
+
         return mossaRandom(tab);
     }
 
@@ -56,11 +66,12 @@ public class Bot {
     }
 
     private int minimax(char[][] tab, int prof, boolean turnoAvv, char bot, char avv, int alpha, int beta) {
-        if (controllo.controllaVittoria(tab, bot)) return 1000;
-        if (controllo.controllaVittoria(tab, avv)) return -1000;
+        if (controlloVittoria(tab, bot)) return 1000 + prof; // meglio vincere subito
+        if (controlloVittoria(tab, avv)) return -1000 - prof; // peggior scenario
+
         if (prof == 0 || pieno(tab)) return 0;
 
-        if (!turnoAvv) {
+        if (!turnoAvv) { // turno bot
             int maxEval = Integer.MIN_VALUE;
             for (int c = 0; c < 7; c++) {
                 if (tab[0][c] == ' ') {
@@ -72,7 +83,7 @@ public class Bot {
                 }
             }
             return maxEval;
-        } else {
+        } else { // turno avversario
             int minEval = Integer.MAX_VALUE;
             for (int c = 0; c < 7; c++) {
                 if (tab[0][c] == ' ') {
@@ -88,10 +99,8 @@ public class Bot {
     }
 
     private boolean pieno(char[][] tab) {
-        for (int i = 0; i < 7; i++){
-            if (tab[0][i] == ' '){
-                return false;
-            }
+        for (int i = 0; i < 7; i++) {
+            if (tab[0][i] == ' ') return false;
         }
         return true;
     }
@@ -113,5 +122,45 @@ public class Bot {
             for (int j = 0; j < orig[i].length; j++)
                 c[i][j] = orig[i][j];
         return c;
+    }
+
+    private boolean controlloVittoria(char[][] tab, char giocatore) {
+        // Orizzontale
+        for (int i = 0; i < tab.length; i++) {
+            int count = 0;
+            for (int j = 0; j < tab[i].length; j++) {
+                if (tab[i][j] == giocatore) count++;
+                else count = 0;
+                if (count == 4) return true;
+            }
+        }
+        // Verticale
+        for (int j = 0; j < tab[0].length; j++) {
+            int count = 0;
+            for (int i = 0; i < tab.length; i++) {
+                if (tab[i][j] == giocatore) count++;
+                else count = 0;
+                if (count == 4) return true;
+            }
+        }
+        // Diagonale \
+        for (int i = 0; i < tab.length - 3; i++) {
+            for (int j = 0; j < tab[0].length - 3; j++) {
+                if (tab[i][j] == giocatore &&
+                    tab[i+1][j+1] == giocatore &&
+                    tab[i+2][j+2] == giocatore &&
+                    tab[i+3][j+3] == giocatore) return true;
+            }
+        }
+        // Diagonale /
+        for (int i = 0; i < tab.length - 3; i++) {
+            for (int j = 3; j < tab[0].length; j++) {
+                if (tab[i][j] == giocatore &&
+                    tab[i+1][j-1] == giocatore &&
+                    tab[i+2][j-2] == giocatore &&
+                    tab[i+3][j-3] == giocatore) return true;
+            }
+        }
+        return false;
     }
 }
