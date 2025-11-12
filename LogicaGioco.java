da97a3296f66deda466fb45ed32377acb06b8b6c
dxvideee <davide.mallardo08@icloud.com>
2025-11-12 18:58:31 +0100

    controlloVittoria finale
    Eliminato il file ControlloVittoria.java e spostato la funzione controlloVittoria dentro LogicaGioco.java. Tenuta la logica dei controlli diagonali perché già corretta, riscrivendola per renderla più chiara
    
    Co-Authored-By: Ssebaaa <205150746+Sssebaaaa@users.noreply.github.com>

--- LogicaGioco.java
+++ LogicaGioco.java
@@ -42,6 +42,59 @@ public class LogicaGioco {
         }
         return true; 
     }
+    
+    public boolean controlloVittoria(char giocatoreCorrente){
+        //controllo orizzontale
+        for(int i=0; i<tabellone.length; i++){
+            int quattroDiFila = 0;
+            for(int j=0; j<tabellone[i].length; j++){
+                if(tabellone[i][j] == giocatoreCorrente){
+                    quattroDiFila++;
+                    if(quattroDiFila == 4){
+                        return true;
+                    } 
+                } else {
+                    quattroDiFila = 0;
+                }
+            }
+        }
+
+        //controllo verticale
+        for(int j=0; j<tabellone[0].length; j++){
+            int quattroDiFila = 0;
+            for(int i=0; i<tabellone.length; i++){
+                if(tabellone[i][j] == giocatoreCorrente){
+                    quattroDiFila++;
+                    if(quattroDiFila == 4){
+                        return true;
+                    } 
+                } else {
+                    quattroDiFila = 0;
+                }
+            }
+        }
+
+        //controllo diagonale 
+        for(int i=0; i<tabellone.length-3; i++){
+            for(int j=0; j<tabellone[0].length -3; j++){
+                if (tabellone[i][j] == giocatoreCorrente && tabellone[i+1][j+1] == giocatoreCorrente && tabellone[i+2][j+2] == giocatoreCorrente && tabellone[i+3][j+3] == giocatoreCorrente) {
+                    return true;
+                }
+            }
+        }
+
+        //controllo diagonale 2
+        for (int i = 0; i < tabellone.length - 3; i++) {
+            for (int j = 3; j < tabellone[0].length; j++) {
+                if (tabellone[i][j] == giocatoreCorrente && tabellone[i+1][j-1] == giocatoreCorrente && tabellone[i+2][j-2] == giocatoreCorrente && tabellone[i+3][j-3] == giocatoreCorrente) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
 
 
 }
