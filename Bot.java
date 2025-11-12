ebc64daeaed0acd7c01681a0b31a244077eb655e
Ssebaaa <nicholassebastiani70@gmail.com>
2025-11-12 18:29:44 +0100

    Update Bot.java
    rifaccio bot.java per creare le 3 difficolta diverse

--- Bot.java
+++ Bot.java
@@ -1,36 +1,15 @@
 public class Bot {
 
-    public int giocaTurnoBot(LogicaGioco logica, char bot, char avversario) {
+    private ControlloVittoria controllo = new ControlloVittoria();
+
+    public int giocaTurnoBot(LogicaGioco logica, char bot, char avversario, String difficolta) {
         char[][] tab = copiaTabella(logica.getTabellone());
-        int colonna;
 
-        for (colonna = 0; colonna < 7; colonna++) {
-            if (colonnaValida(tab, colonna)) {
-                char[][] test = simulaMossa(tab, colonna, bot);
-                if (controllaVittoria(test, bot)) return colonna;
-            }
-        }
+        if (difficolta.equalsIgnoreCase("facile"))
+            return mossaRandom(tab);
 
-        for (colonna = 0; colonna < 7; colonna++) {
-            if (colonnaValida(tab, colonna)) {
-                char[][] test = simulaMossa(tab, colonna, avversario);
-                if (controllaVittoria(test, avversario)) return colonna;
-            }
-        }
+        if (difficolta.equalsIgnoreCase("media"))
+            return mossaMedia(tab, bot, avversario);
 
-        int[] priorita = {3, 2, 4, 1, 5, 0, 6};
-        for (int i = 0; i < priorita.length; i++) {
-            if (colonnaValida(tab, priorita[i])) return priorita[i];
-        }
-
-        for (colonna = 0; colonna < 7; colonna++) {
-            if (colonnaValida(tab, colonna)) return colonna;
-        }
-
-        return -1;
+        return mossaDifficile(tab, bot, avversario);
     }
-
-    
-
-    
-}
