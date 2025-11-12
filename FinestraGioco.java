f993ea9047c166ac0db0044c428ebc66e7ad5cc2
Giulio <cudiciogiulio28@gmail.com>
2025-11-12 18:28:17 +0100

    parteIniz

---
+++ FinestraGioco.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class FinestraGioco extends JFrame{
+    private final int RIGHE=6;
+    private final int COLONNE=7;
+    private final int DIM_CELLA=90;
+}
